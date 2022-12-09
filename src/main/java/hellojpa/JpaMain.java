fa4dbef03b6839a9986fc5c7fb53344145e7b012
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-10 01:24:52 +0900

    patch: various strategy

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -35,8 +35,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+            Item item = em.find(Item.class, movie.getId());
+            System.out.println("findMovie = " + item);
 
 
             tx.commit();
