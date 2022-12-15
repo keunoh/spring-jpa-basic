776f8d6dc1fd10805abec4a4bfc1de028fac5ebf
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-15 23:24:44 +0900

    patch: fetchType

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,17 +15,38 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team2);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member reference = em.getReference(Member.class, member1.getId());
-            System.out.println("reference = " + reference.getClass());
+//            Member m = em.find(Member.class, member1.getId());
 
-            Hibernate.initialize(reference);
+            List<Team> members = em.createQuery("select m from Team m join fetch m.members", Team.class)
+                    .getResultList();
+
+            for (Team member : members) {
+                System.out.println("member = " + member.getName());
+            }
+
+
 
             tx.commit();
         } catch (Exception e) {
